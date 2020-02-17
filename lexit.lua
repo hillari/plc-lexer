bad2d7221c014337b41947757b41b4f470bc4aef
hillari <hillari@gmail.com>
2020-02-16 19:12:18 -0900

    Added util functions and handlers for letter and start. Skeleton for handle_START()

--- lexit.lua
+++ lexit.lua
@@ -108,5 +108,177 @@ local function isPrintableASCII(c)
     end
 end
 
+-- isIllegal
+-- Returns true if string c is an illegal character, false otherwise.
+local function isIllegal(c)
+    if c:len() ~= 1 then
+        return false
+    elseif isWhitespace(c) then
+        return false
+    elseif isPrintableASCII(c) then
+        return false
+    else
+        return true
+    end
+end
+
+
+
+-- *********************************************************************
+-- The lexit
+-- *********************************************************************
+
+
+-- lex
+-- Our lexit
+-- Intended for use in a for-in loop:
+--     for lexstr, cat in lexit.lex(program) do
+-- Here, lexstr is the string form of a lexeme, and cat is a number
+-- representing a lexeme category. (See Public Constants.)
+function lexit.lex(program)
+    -- ***** Variables (like class data members) *****
+
+    local pos       -- Index of next character in program
+                    -- INVARIANT: when getLexeme is called, pos is
+                    --  EITHER the index of the first character of the
+                    --  next lexeme OR program:len()+1
+    local state     -- Current state for our state machine
+    local ch        -- Current character
+    local lexstr    -- The lexeme, so far
+    local prevLex
+    local category  -- Category of lexeme, set when state set to DONE
+    local prevCat
+    local handlers  -- Dispatch table; value created later
+
+    -- ***** States *****
+
+    local DONE      = 0
+    local START     = 1
+    local LETTER    = 2
+    local DIGIT     = 3
+    local DIGEX     = 4
+    local PLUSMINUS = 5
+    local SINGLEQ   = 6
+    local DOUBLEQ   = 7
+
+    -- ***** Character-Related Utility Functions *****
+
+    -- currChar
+    -- Return the current character, at index pos in program. Return
+    -- value is a single-character string, or the empty string if pos is
+    -- past the end.
+    local function currChar()
+        return program:sub(pos, pos)
+    end
+
+    -- nextChar
+    -- Return the next character, at index pos+1 in program. Return
+    -- value is a single-character string, or the empty string if pos+1
+    -- is past the end.
+    local function nextChar()
+        return program:sub(pos+1, pos+1)
+    end
+
+    -- drop1
+    -- Move pos to the next character.
+    local function drop1()
+        pos = pos+1
+    end
+
+    -- add1
+    -- Add the current character to the lexeme, moving pos to the next
+    -- character.
+    local function add1()
+        lexstr = lexstr .. currChar()
+        drop1()
+    end
+
+
+    -- skipWhitespace
+    -- Skip whitespace and comments, moving pos to the beginning of
+    -- the next lexeme, or to program:len()+1.
+    local function skipWhitespace()
+        while true do      -- In whitespace
+            while isWhitespace(currChar()) do
+                drop1()
+            end
+
+            if currChar() ~= "#" then  -- if we only have whitespace, just break
+                break
+            end
+            drop1()
+
+            while true do  -- In comment
+                if currChar() == "" or currChar() == "\n" then -- end of input or newline
+                    drop1()
+                    break
+                end
+                drop1()
+            end
+        end
+    end
+
+    -- lookAhead
+	-- takes a number and returns that number of chars ahead of current position
+    local function lookAhead(n)
+        return program:sub(pos+n, pos+n)
+    end
+
+    -- ***** State-Handler Functions *****
+
+    -- A function with a name like handle_XYZ is the handler function
+    -- for state XYZ
+
+    local function handle_DONE()
+        error("'DONE' state should not be handled\n")
+    end
+
+    local function handle_START()
+    	if isIllegal(ch) then
+    		add1()
+    		state = DONE
+    		category = lexer.MAL
+    	elseif isLetter(ch) or ch == "_" then
+    		add1()
+    		state = LETTER
+    	elseif isDigit(ch) then
+    		add1()
+    		state = DIGIT
+    	elseif ch == "'" or ch == '"' then
+    		add1()
+    		state = STRLITERAL
+
+
+    end
+
+   local function handle_LETTER()
+   		if isLetter(ch) or isDigit(ch) or ch == "_" then --TODO why check for isDigit() here?
+   			add1()
+   		else
+   			state = DONE
+   			if lexstr == "and" or lexstr == "char" or lexstr == "elif" or lexstr == "else" or
+   			lexstr == "end" or lexstr == "false" or lexstr == "func" or lexstr == "if" 
+   			or lexstr == "input" or lexstr == "not" or lexstr == "or" or lexstr == "print"
+   			or lexstr == "return" or lexstr == "true" or lexstr == "while" then
+   				category = lexit.KEY
+   			else
+   				category = lexit.ID
+   			end
+   		end
+   end
+
+
+
+
+
+
+
+
+
+
+
+
+end -- end lexit.lex
+
 
 return lexit
